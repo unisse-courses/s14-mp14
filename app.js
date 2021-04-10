ef3e5fc148858e4ba845b412e35de94bb1cc2c40
JLP603 <60498189+JLP603@users.noreply.github.com>
2021-04-10 18:12:34 +0800

    added db connection stuff?

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const crypto = require("crypto");
+const path = require("path");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
 
 // express app
 const app = express();
-//const db = mongoose.connect('mongodb://Admin:NRcmnt28@ds125068.mlab.com:25068/api-test2');
-
+const db = ('mongodb+srv://Admin:NRcmnt28@lnreader.7e0pp.mongodb.net/test');
+const conn = mongoose.createConnection(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+let gfs;
+conn.once("open", () => {
+    // init stream
+    gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+      bucketName: "uploads"
+    });
+  });
+  //https://dev.to/shubhambattoo/uploading-files-to-mongodb-with-gridfs-and-multer-using-nodejs-5aed
 const PORT = process.env.PORT || 9005;
 
+app.use(express.json());
 app.set('view engine', 'ejs');
 
 
